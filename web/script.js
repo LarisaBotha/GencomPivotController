3b2dd42a740e2d8751c320ce766642313b5c6564
Larisa Botha <larisabotha@gmail.com>
2025-11-09 21:20:57 +0200

    remove temporary test stuff

--- web/script.js
+++ web/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8080"; //"https://pivot-api-of3d.onrender.com";
+const API_URL = "https://pivot-api-of3d.onrender.com"; //"http://127.0.0.1:8080";
 
 // ======================
 // Elements
